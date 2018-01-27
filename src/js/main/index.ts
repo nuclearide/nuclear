eeef8d4ef13eb49a7749462e8fc253abda3d4466
Dan McAtee <danielmcatee@me.com>
2018-01-27 17:01:27 +0100

    Add basic context Menu

--- src/js/main/index.ts
+++ src/js/main/index.ts
@@ -1,10 +1,19 @@
-import {app, BrowserWindow} from 'electron';
+import { app, BrowserWindow, Menu, MenuItem } from "electron";
 
-app.on('ready', () => {
-    const window = new BrowserWindow({
-        width: 600,
-        height: 400
-    });
-    window.loadURL(`file://${__dirname}/../../index.html`);
-    window.maximize();
+app.on("ready", () => {
+  const window = new BrowserWindow({
+    width: 600,
+    height: 400
+  });
+  window.loadURL(`file://${__dirname}/../../index.html`);
+  window.maximize();
+
+  const ctxMenu = new Menu();
+  ctxMenu.append(new MenuItem({ role: "copy" }));
+  ctxMenu.append(new MenuItem({ role: "cut" }));
+  ctxMenu.append(new MenuItem({ role: "paste" }));
+  ctxMenu.append(new MenuItem({ role: "selectall" }));
+  window.webContents.on("context-menu", function(e, params) {
+    ctxMenu.popup(window);
+  });
 });
